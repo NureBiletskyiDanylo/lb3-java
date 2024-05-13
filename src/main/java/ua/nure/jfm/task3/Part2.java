782bf0ea6c9de0fe95c6222c06bd0ae9d5b6de6c
Kirikosha <kirikoshamain@gmail.com>
2024-05-13 16:15:59 +0200

    Finished task 2, broken task 3 (I HATE THIS PIECE OF ******), still yet not fixed task 1.2 and 1.3

--- src/main/java/ua/nure/jfm/task3/Part2.java
+++ src/main/java/ua/nure/jfm/task3/Part2.java
@@ -11,101 +11,81 @@ import java.util.regex.Pattern;
 
 public class Part2 {
 
-	private static final String PATH = "part2.txt";
+    private static final String PATH = "part2.txt";
 
-	public static void main(String[] args) {
-		FileReader reader;
-		try {
-			reader = new FileReader(PATH);
-		} catch (FileNotFoundException exc) {
-			System.out.println("File wasn't found");
-			return;
-		}
+    public static void main(String[] args) {
+        FileReader reader;
+        try {
+            reader = new FileReader(PATH);
+        } catch (FileNotFoundException exc) {
+            System.out.println("File wasn't found");
+            return;
+        }
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String lines = null;
+        try {
+            lines = new String(Files.readAllBytes(Paths.get(PATH)));
+        } catch (IOException exc) {
+            System.out.println("Can't open a file");
+            return;
+        }
+        for (int j = 0; j < 10; j++) {
+            System.out.println(convert(lines, j));
+        }
+    }
 
-		BufferedReader bufferedReader = new BufferedReader(reader);
-		String lines = null;
-		try{lines = new String(Files.readAllBytes(Paths.get(PATH)));}
-		catch (IOException exc)
-		{
-			System.out.println("Can't open a file");
-			return;
-		}
-		for (int j = 0; j < 10; j++) {
-			System.out.println(convert(lines, j));
-		}
-	}
+    public static String convert(String input, int k) {
+        StringBuilder sb = new StringBuilder();
+        String reges = "(?<=[\\s\\n~])([\\p{L},.?!]{"+ k + "})(?=[\\s]|$)";
+        Pattern pattern = Pattern.compile(reges);
+        Matcher matcher = pattern.matcher(input);
+        StringBuilder forExisting = new StringBuilder();
+        while (matcher.find()) {
+            if (sb.length() == 0 && forExisting.length() == 0) {
+                sb.append(k + ": ");
+                sb.append(matcher.group(1));
+                String clearedString = getStringWithoutSpecificSymbols(matcher.group(1));
+                forExisting.append(clearedString);
+                sb.append(" ");
+                continue;
+            }
+            String matchedString = getStringWithoutSpecificSymbols(matcher.group(1));
+            if (forExisting.indexOf(matchedString) != -1) {
+                continue;
+            }
+            sb.append(matcher.group(1));
+            sb.append(" ");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        String result = sb.toString();
+        return result;
+    }
 
-	public static String convert(String input, int k) {
-		StringBuilder sb = new StringBuilder();
-		String reges = "([,. ])([\\w',]{" + k + "})([,. ])";
-		Pattern pattern = Pattern.compile(reges);
-		Matcher matcher = pattern.matcher(input);
-		String[] strings = new String[(int)matcher.results().count()];
-		matcher.reset();
-		int count = 0;
-		while(matcher.find())
-		{
-			if(sb.length() == 0 && count == 0)
-			{
-				sb.append(matcher.group(2));
-				String clearedString = getStringWithoutSpecificSymbols(matcher.group(2));
-				strings[count++] = clearedString;
-				sb.append(" ");
-				continue;
-			}
-			String matchedString = getStringWithoutSpecificSymbols(matcher.group(2));
-			if(contains(strings, matchedString))
-			{
-				continue;
-			}
-			sb.append(matcher.group(2));
-			sb.append(" ");
-		}
-		String result = sb.toString();
-		result.trim();
-		return result;
-	}
+    private static String getStringWithoutSpecificSymbols(String string) {
+        char[] array = string.toCharArray();
+        deleteSpecificChars(array);
+        String normalizedString = new String(array);
+        return normalizedString;
+    }
 
-	private static String getStringWithoutSpecificSymbols(String string)
-	{
-		char[] array = string.toCharArray();
-		deleteSpecificChars(array);
-		String normalizedString = new String(array);
-		return normalizedString;
-	}
-	private static boolean contains(String[] array, String string)
-	{
-		for(int i = 0; i < array.length; i++)
-		{
-			if(string.equals(array[i]))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
+    private static void deleteSpecificChars(char[] array) {
+        int newLen = 0;
+        for (int i = 0; i < array.length; i++) {
+            if ((array[i] >= 'a' && array[i] <= 'z') || (array[i] >= 'A' && array[i] <= 'Z')
+                    || (array[i] >= 'а' && array[i] <= 'я') || (array[i] >= 'А' && array[i] <= 'Я')) {
+                newLen++;
+            }
+        }
 
-	private static void deleteSpecificChars(char[] array) {
-		int newLen = 0;
-		for(int i = 0; i < array.length; i++)
-		{
-			if((array[i] >= 'a' && array[i] <= 'z') || (array[i] >= 'A' && array[i] <= 'Z'))
-			{
-				newLen++;
-			}
-		}
+        char[] newChar = new char[newLen];
 
-		char[] newChar = new char[newLen];
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if ((array[i] >= 'a' && array[i] <= 'z') || (array[i] >= 'A' && array[i] <= 'Z')) {
+                newChar[j++] = array[i];
+            }
+        }
 
-		int j = 0;
-		for(int i = 0; i < array.length; i++)
-		{
-			if((array[i] >= 'a' && array[i] <= 'z') || (array[i] >= 'A' && array[i] <= 'Z'))
-			{
-				newChar[j++] = array[i];
-			}
-		}
-
-		array = newChar;
-	}
+        array = newChar;
+    }
 }
--- src/main/java/ua/nure/jfm/task3/Part3.java
+++ src/main/java/ua/nure/jfm/task3/Part3.java
@@ -1,16 +1,116 @@
 package ua.nure.jfm.task3;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Part3 {
 
-	private static final String PATH = "part3.txt";
+    private static final String PATH = "part3.txt";
 
-	public static void main(String[] args) {
-		String lines = null;
-		System.out.println(convert(lines));
+    public static void main(String[] args) {
+        FileReader reader;
+        try {
+            reader = new FileReader(PATH);
+        } catch (FileNotFoundException exc) {
+            System.out.println("File wasn't found");
+            return;
+        }
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String lines = null;
+        try {
+            lines = new String(Files.readAllBytes(Paths.get(PATH)));
+        } catch (IOException exc) {
+            System.out.println("Can't open a file");
+            return;
+        }
+        System.out.println(convert(lines));
+    }
+
+    public static String convert(String input) {
+        StringBuilder sb = new StringBuilder();
+        String regex = "\\b([\\p{L}]+\\n?)\\b";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            String currentWord = matcher.group(1);
+            String backRegex = "\\b(" + currentWord + "\\n?)\\b";
+            Pattern backPattern = Pattern.compile(backRegex, Pattern.CASE_INSENSITIVE);
+			int ind = input.lastIndexOf(currentWord);
+			if(ind != 0) {
+				String inputUpUntilCurrentWord = input.substring(0, input.indexOf(currentWord) - 1);
+				Matcher backMatcher = backPattern.matcher(inputUpUntilCurrentWord);
+				boolean shouldChangeRegister = countMatches(sb.toString(), currentWord) ;
+				if (shouldChangeRegister) {
+					currentWord = changeRegister(currentWord);
+				}
+			}
+			sb.append(currentWord);
+			sb.append(" ");
+        }
+		sb.deleteCharAt(sb.length() - 1);
+		return sb.toString();
+    }
+
+    private static String changeRegister(String word) {
+        StringBuilder changedCase = new StringBuilder();
+        char[] wordArray = word.toCharArray();
+        for (char letter : wordArray) {
+            if ((letter >= 'A' && letter <= 'Z') || (letter >= 'А' && letter <= 'Я')) {
+                changedCase.append((char)(letter + 32));
+            } else if ((letter >= 'a' && letter <= 'z') || (letter >= 'а' && letter <= 'я')) {
+                changedCase.append((char)(letter - 32));
+            } else if (letter == 'і') {
+                changedCase.append('І');
+            } else if (letter == 'І') {
+                changedCase.append('і');
+            } else {
+                changedCase.append(letter);
+            }
+        }
+		return changedCase.toString();
+    }
+
+	private static boolean countMatches(String input, String word)
+	{
+		StringBuilder loweredStringBuilder = toLower(new StringBuilder(input));
+		String loweredWord = toLower(new StringBuilder(word)).toString();
+		int count = 0;
+		int index = 0;
+
+		while(true)
+		{
+			index = loweredStringBuilder.indexOf(loweredWord, index);
+			count++;
+			if(index == -1) break;
+			index++;
+		}
+		count--;
+
+		return count != 0 && count % 2 != 0;
 	}
 
-	public static String convert(String input) {
-		throw new UnsupportedOperationException();
+	private static StringBuilder toLower(StringBuilder input)
+	{
+		StringBuilder result = new StringBuilder();
+		char[] sbArray = input.toString().toCharArray();
+		for(char letter : sbArray)
+		{
+			if ((letter >= 'A' && letter <= 'Z') || (letter >= 'А' && letter <= 'Я')) {
+				result.append((char)(letter + 32));
+			}
+			else if (letter == 'І') {
+				result.append('і');
+			} else {
+				result.append(letter);
+			}
+		}
+		return result;
 	}
-
 }
+
