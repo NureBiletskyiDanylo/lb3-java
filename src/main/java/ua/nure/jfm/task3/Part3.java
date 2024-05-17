d51cb57441ddfd5f34f39107f76e5140ed6dc135
Kirikosha <kirikoshamain@gmail.com>
2024-05-17 20:28:47 +0200

    finished task

--- src/main/java/ua/nure/jfm/task3/Part3.java
+++ src/main/java/ua/nure/jfm/task3/Part3.java
@@ -9,55 +9,51 @@ import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static ua.nure.jfm.task3.Utils.getContent;
+
 public class Part3 {
 
     private static final String PATH = "part3.txt";
 
+    private static int arraysSizes = 50;
+    private static String[] wordArray = new String[arraysSizes];
+    private static int[] countOfWordsArray = new int[arraysSizes];
+    private static int currentAmountOfDifferentWords = 0;
+
     public static void main(String[] args) {
-        FileReader reader;
-        try {
-            reader = new FileReader(PATH);
-        } catch (FileNotFoundException exc) {
-            System.out.println("File wasn't found");
-            return;
-        }
-        BufferedReader bufferedReader = new BufferedReader(reader);
         String lines = null;
-        try {
-            lines = new String(Files.readAllBytes(Paths.get(PATH)));
-        } catch (IOException exc) {
-            System.out.println("Can't open a file");
-            return;
-        }
+        lines = "Лягає день. Він віддає свої надії ночі. \n" +
+                "Робітники Заморились працювати. \n" +
+                "Там вогні, Яскраві блищуть лампи.\n" +
+                "Там вогні, Яскраві блищуть лампи.\n" +
+                "Робітники Заморились працювати. \n" +
+                "Робітники Заморились працювати."; //getContent(PATH);
         System.out.println(convert(lines));
     }
 
     public static String convert(String input) {
+        //WordsList listOfWords = new WordsList();
         StringBuilder sb = new StringBuilder();
-        String regex = "\\b([\\p{L}]+\\n?)\\b";
-        Pattern pattern = Pattern.compile(regex);
+        String regex = "(\\b\\p{L}+\\b)([,\\s.\\n\\r]*)";
+        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE + Pattern.CASE_INSENSITIVE + Pattern.UNICODE_CHARACTER_CLASS);
         Matcher matcher = pattern.matcher(input);
         while (matcher.find()) {
             String currentWord = matcher.group(1);
-            String backRegex = "\\b(" + currentWord + "\\n?)\\b";
-            Pattern backPattern = Pattern.compile(backRegex, Pattern.CASE_INSENSITIVE);
-			int ind = input.lastIndexOf(currentWord);
-			if(ind != 0) {
-				String inputUpUntilCurrentWord = input.substring(0, input.indexOf(currentWord) - 1);
-				Matcher backMatcher = backPattern.matcher(inputUpUntilCurrentWord);
-				boolean shouldChangeRegister = countMatches(sb.toString(), currentWord) ;
-				if (shouldChangeRegister) {
-					currentWord = changeRegister(currentWord);
-				}
-			}
-			sb.append(currentWord);
-            if(sb.charAt(sb.length() - 1) != '\n')
-            {
-                sb.append(" ");
+            String currentWordInLower = toLower(new StringBuilder(matcher.group(1))).toString();
+            addNewWordForCounting(currentWordInLower);
+            int wordCount = getCountOfWordEntries(currentWordInLower);
+            if (wordCount > 0 && wordCount % 2 == 0) {
+                currentWord = changeRegister(currentWord);
             }
+            sb.append(currentWord);
+            sb.append(matcher.group(2));
         }
-		sb.deleteCharAt(sb.length() - 1);
-		return sb.toString();
+
+        arraysSizes = 50;
+        currentAmountOfDifferentWords = 0;
+        wordArray = new String[arraysSizes];
+        countOfWordsArray = new int[arraysSizes];
+        return sb.toString();
     }
 
     private static String changeRegister(String word) {
@@ -65,9 +61,9 @@ public class Part3 {
         char[] wordArray = word.toCharArray();
         for (char letter : wordArray) {
             if ((letter >= 'A' && letter <= 'Z') || (letter >= 'А' && letter <= 'Я')) {
-                changedCase.append((char)(letter + 32));
+                changedCase.append((char) (letter + 32));
             } else if ((letter >= 'a' && letter <= 'z') || (letter >= 'а' && letter <= 'я')) {
-                changedCase.append((char)(letter - 32));
+                changedCase.append((char) (letter - 32));
             } else if (letter == 'і') {
                 changedCase.append('І');
             } else if (letter == 'І') {
@@ -76,44 +72,73 @@ public class Part3 {
                 changedCase.append(letter);
             }
         }
-		return changedCase.toString();
+        return changedCase.toString();
     }
 
-	private static boolean countMatches(String input, String word)
-	{
-		StringBuilder loweredStringBuilder = toLower(new StringBuilder(input));
-		String loweredWord = toLower(new StringBuilder(word)).toString();
-		int count = 0;
-		int index = 0;
+    private static StringBuilder toLower(StringBuilder input) {
+        StringBuilder result = new StringBuilder();
+        char[] sbArray = input.toString().toCharArray();
+        for (char letter : sbArray) {
+            if ((letter >= 'A' && letter <= 'Z') || (letter >= 'А' && letter <= 'Я')) {
+                result.append((char) (letter + 32));
+            } else if (letter == 'І') {
+                result.append('і');
+            } else if (letter != '\n') {
+                result.append(letter);
+            }
+        }
+        return result;
+    }
 
-		while(true)
-		{
-			index = loweredStringBuilder.indexOf(loweredWord, index);
-			count++;
-			if(index == -1) break;
-			index++;
-		}
-		count--;
+    private static int getCountOfWordEntries(String word) {
+        int index = getIndexOfAWord(word);
+        if (index == -1) {
+            return -1;
+        }
+        return countOfWordsArray[index];
+    }
 
-		return count != 0 && count % 2 != 0;
-	}
+    private static int getIndexOfAWord(String word) {
+        int index = 0;
+        while (index < currentAmountOfDifferentWords) {
+            if (wordArray[index].equals(word)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
 
-	private static StringBuilder toLower(StringBuilder input)
-	{
-		StringBuilder result = new StringBuilder();
-		char[] sbArray = input.toString().toCharArray();
-		for(char letter : sbArray)
-		{
-			if ((letter >= 'A' && letter <= 'Z') || (letter >= 'А' && letter <= 'Я')) {
-				result.append((char)(letter + 32));
-			}
-			else if (letter == 'І') {
-				result.append('і');
-			} else if (letter != '\n'){
-				result.append(letter);
-			}
-		}
-		return result;
-	}
+    private static void addNewWordForCounting(String word) {
+        if (getCountOfWordEntries(word) != -1) {
+            int wordIndex = getIndexOfAWord(word);
+            countOfWordsArray[wordIndex] += 1;
+            return;
+        }
+
+        if (currentAmountOfDifferentWords == countOfWordsArray.length) {
+            increaseArraysSizes();
+        }
+
+        countOfWordsArray[currentAmountOfDifferentWords] = 1;
+        wordArray[currentAmountOfDifferentWords] = word;
+        currentAmountOfDifferentWords++;
+    }
+
+    private static void increaseArraysSizes() {
+        int newArraySizes = arraysSizes * 2;
+        String[] newWordsArray = new String[newArraySizes];
+        int[] newWordsCountArray = new int[newArraySizes];
+        int j = 0;
+        for (int i = 0; i < countOfWordsArray.length; i++) {
+            newWordsCountArray[j] = countOfWordsArray[i];
+            newWordsArray[j] = wordArray[i];
+            j++;
+        }
+
+        countOfWordsArray = newWordsCountArray;
+        wordArray = newWordsArray;
+        arraysSizes = newArraySizes;
+    }
 }
 
--- src/main/java/ua/nure/jfm/task3/converter/CyrillicConverter.java
+++ src/main/java/ua/nure/jfm/task3/converter/CyrillicConverter.java
@@ -3,150 +3,131 @@ package ua.nure.jfm.task3.converter;
 // Cyrillic numerals.
 public class CyrillicConverter {
 
-	private static char[][] digits = {
-			// a - 1	в - 2		г - 3	  д - 4		є - 5	  s - 6		з - 7    и - 8    Ѳ - 9
-			{ '\u0430', '\u0432', '\u0433', '\u0434', '\u0454', '\u0455', '\u0437', '\u0438', '\u0473'},
-			// і - 10	к - 20		л - 30	  м - 40    н - 50    Ѯ - 60    о - 70   п - 80    ч - 90
-			{'\u0456', '\u043A', '\u043B', '\u043C', '\u043D', '\u046F', '\u043E', '\u043F', '\u0447'},
-			// р - 100	с - 200   т - 300   У - 400     ф - 500  х - 600  Ѱ - 700   Ѡ - 800    ц - 900
-			{'\u0440', '\u0441', '\u0442', '\u0443', '\u0444', '\u0445', '\u0471', '\u0461', '\u0446'}
-		};
-	
-	/**
-	 * The thousands sign to multiply the number's value.
-	 */
-	private static char kMul = '\u0482';
-	
-	public static String convert(String str) {
-		StringBuilder clearNumber = getClearNumber(str);
-		int howManyThousands = clearNumber.length() - 3;
-		boolean hasThousands = false;
-		StringBuilder result;
-		if(howManyThousands > 0)
-		{
-			result = convertWithThousands(clearNumber);
-		}
-		else
-		{
-			result = convertWithoutThousands(clearNumber);
-		}
-		return result.toString();
-	}
+    private static char[][] digits = {
+            // a - 1	в - 2		г - 3	  д - 4		є - 5	  s - 6		з - 7    и - 8    Ѳ - 9
+            {'\u0430', '\u0432', '\u0433', '\u0434', '\u0454', '\u0455', '\u0437', '\u0438', '\u0473'},
+            // і - 10	к - 20		л - 30	  м - 40    н - 50    Ѯ - 60    о - 70   п - 80    ч - 90
+            {'\u0456', '\u043A', '\u043B', '\u043C', '\u043D', '\u046F', '\u043E', '\u043F', '\u0447'},
+            // р - 100	с - 200   т - 300   У - 400     ф - 500  х - 600  Ѱ - 700   Ѡ - 800    ц - 900
+            {'\u0440', '\u0441', '\u0442', '\u0443', '\u0444', '\u0445', '\u0471', '\u0461', '\u0446'}
+    };
 
-	public static void main(String[] args) {
-		System.out.println("All the digits:");
-		int k = 1;
-		for (int j = 0; j < 3; j++) {
-			for (int i = 0; i < 9; i++) {
-				System.out.printf("%3s: %s [%s, %s]%n", (i + 1) * k, digits[j][i], j, i);
-			}
-			k *= 10;
-		}
-		System.out.println(kMul);
-		String s = "999_999";
-		System.out.printf("Maximum value (%s):%n", s);
-		System.out.println(CyrillicConverter.convert("20"));
-	}
-	private static int countDigits(String string)
-	{
-		int count = 0;
-		for(int i = 0; i < string.length(); i++)
-		{
-			if(string.charAt(i)>= '0' && string.charAt(i) <= '9')
-			{
-				count++;
-			}
-		}
-		return count;
-	}
-	private static int parseIntC(String string)
-	{
-		int stringDigitsLength = countDigits(string);
-		int number = 0;
-		int currentNumberIndex = 0;
-		for(int i = 0; i < string.length(); i++)
-		{
-			if(string.charAt(i)>= '0' && string.charAt(i) <= '9')
-			{
-				int currentNumber = (int) string.charAt(i) - '0';
-				int power = (int)Math.pow(10, (stringDigitsLength - currentNumberIndex - 1));
-				number+= currentNumber * power;
-				currentNumberIndex++;
-			}
-		}
-		return number;
-	}
+    /**
+     * The thousands sign to multiply the number's value.
+     */
+    private static char kMul = '\u0482';
 
-	private static StringBuilder getClearNumber(String str)
-	{
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < str.length(); i++)
-		{
-			if(str.charAt(i) >= '0' && str.charAt(i) <= '9')
-			{
-				sb.append(str.charAt(i));
-			}
-		}
-		return sb;
-	}
+    public static String convert(String str) {
+        StringBuilder clearNumber = getClearNumber(str);
+        int howManyThousands = clearNumber.length() - 3;
+        boolean hasThousands = false;
+        StringBuilder result;
+        if (howManyThousands > 0) {
+            result = convertWithThousands(clearNumber);
+        } else {
+            result = convertWithoutThousands(clearNumber);
+        }
+        return result.toString();
+    }
 
-	private static StringBuilder convertWithThousands(StringBuilder number)
-	{
-		StringBuilder result = new StringBuilder();
-		int numberPowerIndex = 0;
-		int index = number.length() - 1;
-		while(index >= (number.length() - 3))
-		{
+    public static void main(String[] args) {
+        System.out.println("All the digits:");
+        int k = 1;
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 9; i++) {
+                System.out.printf("%3s: %s [%s, %s]%n", (i + 1) * k, digits[j][i], j, i);
+            }
+            k *= 10;
+        }
+        System.out.println(kMul);
+        String s = "999_999";
+        System.out.printf("Maximum value (%s):%n", s);
+        System.out.println(CyrillicConverter.convert("20"));
+    }
 
-			if(number.charAt(index) == '0')
-			{
-				numberPowerIndex++;
-				index--;
-				continue;
-			}
-			int charPositionInDigits = (number.charAt(index) - '0') - 1;
-			result.insert(0, digits[numberPowerIndex++][charPositionInDigits]);
-			index--;
-		}
+    private static int countDigits(String string) {
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) >= '0' && string.charAt(i) <= '9') {
+                count++;
+            }
+        }
+        return count;
+    }
 
-		while(index >=0)
-		{
-			if(numberPowerIndex > 2)
-			{
-				numberPowerIndex = 0;
-			}
-			if(number.charAt(index) == '0')
-			{
-				numberPowerIndex++;
-				index--;
-				continue;
-			}
-			int charPositionInDigits = (number.charAt(index) - '0') - 1;
-			result.insert(0, digits[numberPowerIndex++][charPositionInDigits]);
-			index--;
-			result.insert(0, kMul);
-		}
-		return result;
-	}
+    private static int parseIntC(String string) {
+        int stringDigitsLength = countDigits(string);
+        int number = 0;
+        int currentNumberIndex = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) >= '0' && string.charAt(i) <= '9') {
+                int currentNumber = (int) string.charAt(i) - '0';
+                int power = (int) Math.pow(10, (stringDigitsLength - currentNumberIndex - 1));
+                number += currentNumber * power;
+                currentNumberIndex++;
+            }
+        }
+        return number;
+    }
 
-	private static StringBuilder convertWithoutThousands(StringBuilder number)
-	{
-		StringBuilder result = new StringBuilder();
-		int numberPowerIndex = 0;
-		int index = number.length() - 1;
+    private static StringBuilder getClearNumber(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb;
+    }
 
-		while(index >=0)
-		{
-			if(number.charAt(index) == '0')
-			{
-				numberPowerIndex++;
-				index--;
-				continue;
-			}
-			int charPositionInDigits = (number.charAt(index) - '0') - 1;
-			result.insert(0, digits[numberPowerIndex++][charPositionInDigits]);
-			index--;
-		}
-		return result;
-	}
+    private static StringBuilder convertWithThousands(StringBuilder number) {
+        StringBuilder result = new StringBuilder();
+        int numberPowerIndex = 0;
+        int index = number.length() - 1;
+        while (index >= (number.length() - 3)) {
+
+            if (number.charAt(index) == '0') {
+                numberPowerIndex++;
+                index--;
+                continue;
+            }
+            int charPositionInDigits = (number.charAt(index) - '0') - 1;
+            result.insert(0, digits[numberPowerIndex++][charPositionInDigits]);
+            index--;
+        }
+
+        while (index >= 0) {
+            if (numberPowerIndex > 2) {
+                numberPowerIndex = 0;
+            }
+            if (number.charAt(index) == '0') {
+                numberPowerIndex++;
+                index--;
+                continue;
+            }
+            int charPositionInDigits = (number.charAt(index) - '0') - 1;
+            result.insert(0, digits[numberPowerIndex++][charPositionInDigits]);
+            index--;
+            result.insert(0, kMul);
+        }
+        return result;
+    }
+
+    private static StringBuilder convertWithoutThousands(StringBuilder number) {
+        StringBuilder result = new StringBuilder();
+        int numberPowerIndex = 0;
+        int index = number.length() - 1;
+
+        while (index >= 0) {
+            if (number.charAt(index) == '0') {
+                numberPowerIndex++;
+                index--;
+                continue;
+            }
+            int charPositionInDigits = (number.charAt(index) - '0') - 1;
+            result.insert(0, digits[numberPowerIndex++][charPositionInDigits]);
+            index--;
+        }
+        return result;
+    }
 }
